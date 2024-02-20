28f2423c35a04a955f3d913a418cfa1dcbc0bbd7
Patrick Burrows <patrick.burrows@ukg.com>
2024-02-20 15:12:38 -0500

    add google analytics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,105 +1,109 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Big Picture Project Collaboration',
-  tagline: 'The all-in-one collaboration tool for businesses and projects',
-  favicon: 'img/logo-16.png',
+    title: 'Big Picture Project Collaboration',
+    tagline: 'The all-in-one collaboration tool for businesses and projects',
+    favicon: 'img/logo-16.png',
 
-  // Set the production url of your site here
-  url: 'https://bigpicture.site',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-  trailingSlash: false,
+    // Set the production url of your site here
+    url: 'https://bigpicture.site',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+    trailingSlash: false,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'BigPicturePM', // Usually your GitHub org/user name.
-  projectName: 'bp-site', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'BigPicturePM', // Usually your GitHub org/user name.
+    projectName: 'bp-site', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  plugins: ['docusaurus-plugin-sass'],
+    plugins: ['docusaurus-plugin-sass'],
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                gtag: {
+                    trackingID: 'G-7LCXCX19L9',
+                    anonymizeIP: true,
+                },
+                blog: {
+                    showReadingTime: true,
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    colorMode: {
-      defaultMode: 'dark',
-      disableSwitch: false,
-      respectPrefersColorScheme: true
-    },
-    navbar: {
-      title: 'Big Picture',
-      logo: {
-        alt: 'Big Picture Logo',
-        src: 'img/logo-empty.svg',
-      },
-      items: [
-        { to: '/blog', label: 'Blog', position: 'left' },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Substack',
-              href: 'https://bigpictureprojects.substack.com',
-            },
-          ],
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: false,
+            respectPrefersColorScheme: true
         },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
+        navbar: {
+            title: 'Big Picture',
+            logo: {
+                alt: 'Big Picture Logo',
+                src: 'img/logo-empty.svg',
             },
-          ],
+            items: [
+                {to: '/blog', label: 'Blog', position: 'left'},
+            ],
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Big Picture`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Substack',
+                            href: 'https://bigpictureprojects.substack.com',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Big Picture`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
